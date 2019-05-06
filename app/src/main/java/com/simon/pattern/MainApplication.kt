57d021184613ebdebc483d56ccdae6817710adcd
ZadorozhnyiS <zadorozhnyysemen@gmail.com>
2019-05-07 02:56:55 +0300

    Added logging

--- app/src/main/java/com/simon/pattern/MainApplication.kt
+++ app/src/main/java/com/simon/pattern/MainApplication.kt
@@ -1,10 +1,37 @@
 package com.simon.pattern
 
+import android.util.Log
 import com.simon.pattern.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+import timber.log.Timber
 
 class MainApplication : DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
+            Timber.plant(CrashlyticsTree())
+        }
+    }
+
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
         DaggerAppComponent.builder().create(this)
+
+    companion object {
+        private class CrashlyticsTree : Timber.Tree() {
+            override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                when (priority) {
+                    Log.DEBUG, Log.VERBOSE -> {
+                    }
+                    else -> {
+                        // TODO log to firebase
+                    }
+                }
+            }
+        }
+    }
 }
